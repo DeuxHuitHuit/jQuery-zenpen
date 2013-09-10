02ed62354f757e1db157ada07fe0f9b5ddd81e60
Nicolas Brassard <nico@nitriques.com>
2013-09-10 00:16:29 -0400

    jQuery plugin bootstrap code

---
+++ src/js/jquery.zenpen.js
@@ -0,0 +1,106 @@
+
+(function ($) {
+	
+	if (!!$.zenpen) {
+		return;	
+	}
+	
+	var focus = function (elem) {
+		var range = document.createRange();
+		var selection = window.getSelection();
+		range.setStart(elem.get(0), 1);
+		selection.removeAllRanges();
+		selection.addRange(range);
+	};
+	
+	var createButtonFactory = function (cla, text) {
+		return createButton = function () {
+			var b = $('<button />').addClass(cla).text(text);
+			var ctx = this;
+			return b;
+		};
+	};
+	
+	var createPopUp = function (actions) {
+		var popup = $('<div />').addClass('zenpen-panel');
+		var options = $('<div />').addClass('zenpen-options');
+		$.each(actions, function (i, a) {
+			if (!!a) {
+				var action = $.zenpen.actions[a];
+				options.append(action.create());
+			}
+		});
+		return popup.append(options);
+	};
+
+	$.fn.zenpen = function (options) {
+		options = $.extend(true, {}, $.zenpen.defaults, options);
+		
+		if (!$.isArray(options.actions)) {
+			options.actions = options.actions.split(',');
+		}
+		
+		var t = $(this);
+		
+		var init = function (index, elem) {
+			elem = $(elem);
+			
+			var popup = createPopUp(options.actions);
+			var wrap = $('<div />').addClass('zenpen-wrap');
+			
+			elem.wrap(wrap);
+			elem.after(popup);
+			
+			focus(elem);
+		};
+		
+		t.each(init);
+	};
+
+	$.zenpen = function (target) {
+		target = target || $.zenpen.defaults.autoLoadSelector;
+		target = $(target);
+		target.zenpen();
+	};
+	
+	$.zenpen.actions = {
+		bold: {
+			create: createButtonFactory('bold', 'b'),
+			exec: $.noop
+		},
+		italic: {
+			create: createButtonFactory('italic', 'i'),
+			exec: $.noop	
+		},
+		url: {
+			create: function () {
+				var wrap = $('<span />');
+				var input = $('<input />').addClass('url-input')
+					.attr('type','text')
+					.attr('placeholder','Type or Paste URL here');
+				wrap.append(createButtonFactory('url useicons','&#xe005;')());
+				return wrap;
+			},
+			exec: $.noop
+		},
+		quote: {
+			create: createButtonFactory('quote', '&rdquo;'),
+			exec: $.noop
+		}
+	};
+	
+	$.zenpen.defaults = {
+		autoLoad: true,
+		autoLoadSelector: '*[contenteditable="true"]',
+		actions: 'url,bold,italic,quote',
+	};
+	
+	var autoLoad = function () {
+		if (!!$.zenpen.defaults.autoLoad) {
+			$.zenpen();
+		}
+	};
+	
+	$(autoLoad);
+	
+})(jQuery);
