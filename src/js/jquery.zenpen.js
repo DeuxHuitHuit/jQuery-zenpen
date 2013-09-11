76822366ee19cf0e43fe36083a5c5dd842cca4c8
Nicolas Brassard <nico@nitriques.com>
2013-09-11 00:00:35 -0400

    Start to migrate code from zenpen editor.js

--- src/js/jquery.zenpen.js
+++ src/js/jquery.zenpen.js
@@ -5,12 +5,21 @@
 		return;	
 	}
 	
+	var counter = 0;
+	
 	var focus = function (elem) {
 		var range = document.createRange();
 		var selection = window.getSelection();
 		range.setStart(elem.get(0), 1);
 		selection.removeAllRanges();
 		selection.addRange(range);
+	};
+	
+	var execCommandFactory = function () {
+		var a = arguments;
+		return execCommand = function () {
+			document.execCommand(a);	
+		};	
 	};
 	
 	var createButtonFactory = function (cla, text) {
@@ -45,12 +54,122 @@
 		var init = function (index, elem) {
 			elem = $(elem);
 			
+			var id = (++counter);
+			var namespace = '.zenpen-' + id;
 			var popup = createPopUp(options.actions);
 			var wrap = $('<div />').addClass('zenpen-wrap');
+			var scrollTimeout = 0;
+			var lastType = false;
+			
+			var checkTextHighlighting = function (event) {
+
+				var selection = window.getSelection();
+		
+				if ( (event.target.className === "url-input" ||
+				     event.target.classList.contains( "url" ) ||
+				     event.target.parentNode.classList.contains( "ui-inputs")) ) {
+		
+					currentNodeList = findNodes( selection.focusNode );
+					updateBubbleStates();
+					return;
+				}
+		
+				// Check selections exist
+				if ( selection.isCollapsed === true && lastType === false ) {
+		
+					onSelectorBlur();
+				}
+		
+				// Text is selected
+				if ( selection.isCollapsed === false ) {
+		
+					currentNodeList = findNodes( selection.focusNode );
+		
+					// Find if highlighting is in the editable area
+					if ( hasNode( currentNodeList, "ARTICLE") ) {
+						updateBubbleStates();
+						updateBubblePosition();
+		
+						// Show the ui bubble
+						textOptions.className = "text-options active";
+					}
+				}
+		
+				lastType = selection.isCollapsed;
+			};
+
+			var updateBubblePosition = function () {
+				var selection = window.getSelection();
+				var range = selection.getRangeAt(0);
+				var boundary = range.getBoundingClientRect();
+				
+				textOptions.style.top = boundary.top - 5 + window.pageYOffset + "px";
+				textOptions.style.left = (boundary.left + boundary.right)/2 + "px";
+			};
+		
+			var updateBubbleStates = function () {
+		
+				// It would be possible to use classList here, but I feel that the
+				// browser support isn't quite there, and this functionality doesn't
+				// warrent a shim.
+		
+				if ( hasNode( currentNodeList, 'B') ) {
+					boldButton.className = "bold active"
+				} else {
+					boldButton.className = "bold"
+				}
+		
+				if ( hasNode( currentNodeList, 'I') ) {
+					italicButton.className = "italic active"
+				} else {
+					italicButton.className = "italic"
+				}
+		
+				if ( hasNode( currentNodeList, 'BLOCKQUOTE') ) {
+					quoteButton.className = "quote active"
+				} else {
+					quoteButton.className = "quote"
+				}
+		
+				if ( hasNode( currentNodeList, 'A') ) {
+					urlButton.className = "url useicons active"
+				} else {
+					urlButton.className = "url useicons"
+				}
+			};
+		
+			var onSelectorBlur = function () {
+		
+				textOptions.className = "text-options fade";
+				setTimeout( function() {
+		
+					if (textOptions.className == "text-options fade") {
+		
+						textOptions.className = "text-options";
+						textOptions.style.top = '-999px';
+						textOptions.style.left = '-999px';
+					}
+				}, 260 )
+			};
+
 			
 			elem.wrap(wrap);
 			elem.after(popup);
+			elem.attr('data-zenpen', id);
 			
+			elem
+				.on('keyup' + namespace, checkTextHighlighting)
+				.on('mousedown' + namespace, checkTextHighlighting)
+				.on('mouseup' + namespace, checkTextHighlighting);
+				
+			wrap.on('scroll' + namespace, function() {
+				if ( !!scrollTimeout ) {
+					clearTimeout(scrollTimeout);
+					scrollTimeout = 0;
+				}
+				scrollTimeout = setTimeout(updateBubblePosition, 100);
+			});
+							
 			focus(elem);
 		};
 		
@@ -66,11 +185,11 @@
 	$.zenpen.actions = {
 		bold: {
 			create: createButtonFactory('bold', 'b'),
-			exec: $.noop
+			exec: execCommandFactory('bold', false )
 		},
 		italic: {
 			create: createButtonFactory('italic', 'i'),
-			exec: $.noop	
+			exec: execCommandFactory('italic', false )	
 		},
 		url: {
 			create: function () {
@@ -81,11 +200,38 @@
 				
 				return btn.add(input);
 			},
-			exec: $.noop
+			exec: function ( url ) {
+
+				rehighlightLastSelection();
+		
+				// Unlink any current links
+				document.execCommand( 'unlink', false );
+		
+				if (url !== "") {
+				
+					// Insert HTTP if it doesn't exist.
+					if ( !url.match("^(http|https)://") ) {
+		
+						url = "http://" + url;	
+					} 
+		
+					document.execCommand( 'createLink', false, url );
+				}
+			}
 		},
 		quote: {
 			create: createButtonFactory('quote', '&rdquo;'),
-			exec: $.noop
+			exec: function () {
+		
+				var nodeNames = findNodes( window.getSelection().focusNode );
+		
+				if ( hasNode( nodeNames, 'BLOCKQUOTE' ) ) {
+					document.execCommand( 'formatBlock', false, 'p' );
+					document.execCommand( 'outdent' );
+				} else {
+					document.execCommand( 'formatBlock', false, 'blockquote' );
+				}
+			}
 		}
 	};
 	
@@ -101,6 +247,12 @@
 		}
 	};
 	
+	var resize = function () {
+			
+	};
+	
 	$(autoLoad);
+	
+	$(window).resize(resize);
 	
 })(jQuery);
